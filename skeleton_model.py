5905b2260670c3ed0adef31fd58f12a516974dfa
paintception <matthia.sabatelli@gmail.com>
2016-06-30 22:01:53 +0200

    basic scores for genes and generations added

--- skeleton_model.py
+++ skeleton_model.py
@@ -3,6 +3,21 @@ import random
 import pyevolve
 from matplotlib import pyplot as plt
 
+#TODO Think about the evaluation function of the chromosomes
+#for the genetic algorithm   
+
+#TODO Implement the genetic algorithm itself
+
+"""
+Check this stuff if it makes sense!!!
+
+max_Beliefs = 22
+max_Intentions = 22 
+max_Desire = 20
+
+Optimal_Score = 64
+"""
+
 def makeBeliefs(): #First part of binary string
         
     g = random.randint(1,10)  
@@ -63,59 +78,53 @@ def createChromosome(): #Creates one single Chromosome of the population
 def genAlgorithm(genetic_pool): 
     pass
 
-def calculateBelief(gamma, psi, alfa):
 
-    gamma = int(gamma,2)
-    psi = int(psi,2)
-    alfa = int(alfa,2)
+def splitGenesAndScores(Chromosome):
 
-    #return BeliefVal
-
-def calculateIntention(gamma, psi, alfa):
-    
-    gamma = int(gamma,2)
-    psi = int(psi,2)
-    alfa = int(alfa,2)
-    #return IntentionVal
-
-def calculateDesire(gamma, psi, alfa):
-    
-    gamma = int(gamma,2)
-    psi = int(psi,2)
-    alfa = int(alfa,2)
-    #return DesireVal
-
-def splitGenes(Chromosome):
-
-    #print Chromosome
+    #print "This is one single Agent:", Chromosome
 
     B_gamma = Chromosome[:4]
     B_psi = Chromosome[4:8]
     B_alfa = Chromosome[8:12]
     
-    calculateBelief(B_gamma, B_psi, B_alfa)
+    Score_B_gamma = int(B_gamma,2)
+    Score_B_psi = int(B_psi,2)
+    Score_B_alfa = int(B_alfa,2)
 
+    BeliefScore = Score_B_gamma + Score_B_psi + Score_B_alfa   
+    
+    #print "These are the Belief features:", B_gamma, B_psi, B_alfa
+    
     I_gamma = Chromosome[12:16]
     I_psi = Chromosome[16:20]
     I_alfa = Chromosome[20:24]
 
-    calculateIntention(I_gamma, I_psi, I_alfa)
+    Score_I_gamma = int(I_gamma,2)
+    Score_I_psi = int(I_psi,2)
+    Score_I_alfa = int(I_alfa,2)
 
+    IntentionScore = Score_I_gamma + Score_I_psi + Score_I_alfa    
+
+    #print "These are the Intention features:", I_gamma, I_psi, I_alfa
+    
     D_gamma = Chromosome[24:28]
     D_psi = Chromosome[28:32]
     D_alfa = Chromosome[32:36]
 
-    calculateDesire(D_gamma, D_psi, D_alfa)
-    
+    Score_D_gamma = int(D_gamma,2)
+    Score_D_psi = int(D_psi,2)
+    Score_D_alfa = int(D_alfa,2)
 
-def splitPool(genetic_pool):    #How good the Sender Chromosomes are
+    DesireScore = Score_D_gamma + Score_D_psi + Score_D_alfa   
     
-    #print genetic_pool
+    #print "These are the Desire features:", D_gamma, D_psi, D_alfa
     
-    for i in genetic_pool:
-        splitGenes(i)
-    
-    
+    ChromosomeScore = BeliefScore + IntentionScore + DesireScore
+    #print "----------------------------------------------------"
+      
+
+    return Chromosome, ChromosomeScore
+
 def createPopulation(num_pop): #List of chromosomes 
      
     population = []
@@ -127,7 +136,23 @@ def createPopulation(num_pop): #List of chromosomes
     return population        
         
 if __name__ == "__main__":
-            
-    genetic_pool = createPopulation(3)
-    splitPool(genetic_pool)
-    
+
+    number_generations = 2
+    scores = []
+
+    for gen in xrange(0,number_generations):
+
+        print "This is generation:", gen
+
+        genetic_pool = createPopulation(2)
+        
+        print "This is the genetic pool:", genetic_pool
+        tot = 0    
+        for i in genetic_pool:
+            c, s = splitGenesAndScores(i)
+            print "This is the Chromosome", c
+            print "This is his Score", s
+            tot += s
+            average_score = tot/2
+        scores.append(average_score)
+
