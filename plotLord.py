39b3c3ae110998bad99e04ac913a496ca4d640be
paintception <matthia.sabatelli@gmail.com>
2016-07-14 14:23:33 +0200

    start plots booooring

--- plotLord.py
+++ plotLord.py
@@ -1,4 +1,5 @@
 from matplotlib import pyplot as plt
+from numpy.random import normal
 import random
 
 def succes_plot(generations, standardscores):
@@ -11,10 +12,27 @@ def succes_plot(generations, standardscores):
 	plt.plot(generations, standardscores)
 	plt.show()
 
-def split_for_plotting(w_gene, b_gene):
-	pass
+def split_for_plotting(gene):
+	
+	Beliefs = gene[:12]
+	Intentions = gene[12:24] 
+	Desires = gene[24:]
+
+	gamma_B = int(Beliefs[:4],2)
+	psi_B = int(Beliefs[4:8],2)
+	alfa_B = int(Beliefs[8:12],2)
+
+	gamma_I = int(Intentions[:4],2)
+	psi_I = int(Intentions[4:8],2)
+	alfa_I = int(Intentions[8:12],2)
+
+	gamma_D = int(Desires[:4],2)
+	psi_D = int(Desires[4:8],2)
+	alfa_D = int(Desires[8:12],2)
+
 
 def individual_stats(worst, best):
+	
 	print "==================================================="
 	min_index = random.randrange(len(worst))
 	Worst_gene_to_analyze = worst[min_index]
@@ -24,4 +42,5 @@ def individual_stats(worst, best):
 	Best_gene_to_analyze = best[min_index]
 	print "Best Gene", Best_gene_to_analyze
 	
-	split_for_plotting(Worst_gene_to_analyze, Best_gene_to_analyze)
+	split_for_plotting(Worst_gene_to_analyze)
+	split_for_plotting(Best_gene_to_analyze)
